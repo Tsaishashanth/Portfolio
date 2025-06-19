ec4e58d37782ae2a300ecd4cafd55dd18d92e2ad
Tsaishashanth <saishashanthtarigopula@gmail.com>
2025-06-19 18:43:51 +0530

    added resume and luma board js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openResume() {
+    window.open('sai_shashanth-BMSCE-resume.pdf', '_blank');
+  }
+
+function openlumaboard(){
+    window.open('https://lumaboard.vercel.app/', '_blank');
+}
